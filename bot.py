cd15d6aa65b84204acd2640d9ffb69288c4fa42d
escamillahector129-jpg <escamillahector129@gmail.com>
2026-06-29 12:16:38 -0600

    Implement Discord bot with basic commands
    This bot responds to messages and has commands for ping and user info.

---
+++ bot.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('MTUyMTIxNjY2NDEwMjc2ODk1Mg.GlFCIm.ePY0eXIlHT7IQ2cMnkOcyGlTjfM-NMHCSBPL7A')
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} se ha conectado a Discord!')
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    
+    if message.content.lower() == 'hola':
+        await message.channel.send(f'¡Hola {message.author.name}! 👋')
+    
+    await bot.process_commands(message)
+
+@bot.command(name='ping')
+async def ping(ctx):
+    await ctx.send(f'🏓 Pong! Latencia: {round(bot.latency * 1000)}ms')
+
+@bot.command(name='usuario')
+async def usuario(ctx):
+    embed = discord.Embed(title=f"Información de {ctx.author.name}", color=discord.Color.green())
+    embed.add_field(name="ID", value=ctx.author.id, inline=False)
+    await ctx.send(embed=embed)
+
+bot.run(MTUyMTIxNjY2NDEwMjc2ODk1Mg.GlFCIm.ePY0eXIlHT7IQ2cMnkOcyGlTjfM-NMHCSBPL7A)
